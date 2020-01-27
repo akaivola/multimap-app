ed6b1e208cc7c35e4980feb591c5fb3d01dd42d5
Aki Kaivola <aki.kaivola@gmail.com>
2020-01-27 17:15:24 +0200

    update packages

--- env/dev/env/index.cljs
+++ env/dev/env/index.cljs
@@ -5,6 +5,6 @@
 (set! js/window.goog js/undefined)
 
 (-> (js/require "../../../js/figwheel-bridge")
-    (.withModules #js {"react-native" (js/require "react-native"), "expo" (js/require "expo"), "@expo/vector-icons" (js/require "@expo/vector-icons"), "react" (js/require "react"), "create-react-class" (js/require "create-react-class"), "./assets/images/cljs.png" (js/require "../../../assets/images/cljs.png"), "./assets/icons/app.png" (js/require "../../../assets/icons/app.png"), "./assets/icons/loading.png" (js/require "../../../assets/icons/loading.png")}
+    (.withModules #js {"./assets/icons/loading.png" (js/require "../../../assets/icons/loading.png"), "@eva-design/eva" (js/require "@eva-design/eva"), "expo" (js/require "expo"), "./assets/images/cljs.png" (js/require "../../../assets/images/cljs.png"), "./assets/icons/app.png" (js/require "../../../assets/icons/app.png"), "@ui-kitten/components" (js/require "@ui-kitten/components"), "react-native" (js/require "react-native"), "react" (js/require "react"), "create-react-class" (js/require "create-react-class")}
 )
     (.start "main" "expo" "192.168.10.47"))
